df383ef9f108e94fd64b81cc1e392c0fb8e473fc
Narcizo <narcizo.gabriel2@gmail.com>
2023-09-20 23:37:09 -0300

    Sim, eu vou participar da rinha de compiladores. Não, eu não estou são.

---
+++ src/interpreter.ts
@@ -0,0 +1,82 @@
+import jsonData from "./../source.rinha.json";
+
+let varMap = new Map<string, any>();
+
+//TODO usar Jest para testes unitários
+
+// console.log(jsonData);
+console.log("---------------------");
+
+interpreter(jsonData.expression);
+
+function interpreter(node: any) : any{
+  switch (node.kind) {
+    case "Print":
+      console.log("Print");
+      const print = interpreter(node.value);
+      console.log(print);
+      break;
+    case "Int":
+      console.log("Int");
+      return node.value;
+    case "Str":
+      console.log("Str");
+      return node.value;
+    case "Bool":
+      console.log("Bool");
+      return node.value;
+    case "Var":
+      return varMap.get(node.text);
+    case "Let":
+      console.log("Let");
+      const varValue = interpreter(node.value);
+      varMap.set(node.name.text, varValue);
+      interpreter(node.next);
+      break;
+    case "Binary":
+      console.log("Binary");
+      return operate(node.op , interpreter(node.lhs), interpreter(node.rhs));
+    case "If":
+      console.log("If");
+      if (interpreter(node.condition)) {
+        interpreter(node.then);
+      } else {
+        interpreter(node.otherwise);
+      }
+      break;
+  }
+}
+
+function operate(op: string, lhs: any, rhs: any) {
+  console.log("operate()");
+
+  // Que bagulho horrivel
+  switch (op) {
+    case "Add":
+      return lhs + rhs;
+    case "Sub":
+      return lhs - rhs;
+    case "Mult":
+      return lhs * rhs;
+    case "Div":
+      return lhs / rhs;
+    case "Rem":
+      return lhs % rhs;
+    case "Eq":
+      return lhs == rhs;
+    case "Neq":
+      return lhs != rhs;
+    case "Lt":
+      return lhs < rhs;
+    case "Gt":
+      return lhs > rhs;
+    case "Lte":
+      return lhs <= rhs;
+    case "Gte":
+      return lhs >= rhs;
+    case "And":
+      return lhs && rhs;
+    case "Or":
+      return lhs || rhs;
+  }
+}
